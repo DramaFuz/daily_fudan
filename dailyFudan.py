ee6d7f49b320d54eab14a787ad90e8d7e3d21db8
shadowqwq <dracarys3057@gmail.com>
2021-05-05 17:25:40 +0800

    Initial version.

--- dailyFudan.py
+++ dailyFudan.py
@@ -1,26 +1,23 @@
 import time
 from json import loads as json_loads
-from os import path as os_path
-from sys import exit as sys_exit
 from sys import argv as sys_argv
-
 from lxml import etree
 from requests import session
+import smtplib
+from email.mime.text import MIMEText
 import logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
+SUCCESS = False
 
 class Fudan:
     """
     建立与复旦服务器的会话，执行登录/登出操作
     """
-    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0"
+    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
 
     # 初始化会话
-    def __init__(self,
-                 uid, psw,
-                 url_login='https://uis.fudan.edu.cn/authserver/login'):
+    def __init__(self, uid, psw, url_login='https://uis.fudan.edu.cn/authserver/login'):
         """
         初始化一个session，及登录信息
         :param uid: 学号
@@ -39,17 +36,17 @@ class Fudan:
         检查是否能打开登录页面
         :return: 登录页page source
         """
-        logging.debug("Initiating——")
+        logging.info("Initiating...")
         page_login = self.session.get(self.url_login)
 
-        logging.debug("return status code " + str(page_login.status_code))
+        logging.debug("Response code: " + str(page_login.status_code))
 
         if page_login.status_code == 200:
-            logging.debug("Initiated——")
+            logging.info("Initiated.")
             return page_login.text
         else:
-            logging.debug("Fail to open Login Page, Check your Internet connection\n")
-            self.close()
+            logging.error("Failed to open the login page.")
+            raise Exception("Failed to open the login page.")
 
     def login(self):
         """
@@ -57,14 +54,14 @@ class Fudan:
         """
         page_login = self._page_init()
 
-        logging.debug("parsing Login page——")
+        logging.debug("Parsing the login page...")
         html = etree.HTML(page_login, etree.HTMLParser())
 
-        logging.debug("getting tokens")
+        logging.debug("Getting tokens...")
         data = {
             "username": self.uid,
             "password": self.psw,
-            "service" : "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
+            # "service" : "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
         }
 
         # 获取登录页上的令牌
@@ -82,20 +79,20 @@ class Fudan:
             "User-Agent": self.UA
         }
 
-        logging.debug("Login ing——")
+        logging.info("Logining...")
         post = self.session.post(
                 self.url_login,
                 data=data,
                 headers=headers,
                 allow_redirects=False)
 
-        logging.debug("return status code %d" % post.status_code)
+        logging.debug("Response code: %d" % post.status_code)
 
         if post.status_code == 302:
-            logging.debug("登录成功")
+            logging.info("Login.")
         else:
-            logging.debug("登录失败，请检查账号信息")
-            self.close()
+            logging.error("Failed to login.")
+            raise Exception("Failed to login.")
 
     def logout(self):
         """
@@ -105,9 +102,10 @@ class Fudan:
         expire = self.session.get(exit_url).headers.get('Set-Cookie')
 
         if '01-Jan-1970' in expire:
-            logging.debug("登出完毕")
+            logging.info("Logout.")
         else:
-            logging.debug("登出异常")
+            logging.error("Failed to logout.")
+            raise Exception("Failed to logout.")
 
     def close(self):
         """
@@ -115,8 +113,7 @@ class Fudan:
         """
         self.logout()
         self.session.close()
-        logging.debug("关闭会话")
-        sys_exit()
+        logging.info("Session closed.")
 
 class Zlapp(Fudan):
     last_info = ''
@@ -125,56 +122,57 @@ class Zlapp(Fudan):
         """
         检查
         """
-        logging.debug("检测是否已提交")
-        get_info = self.session.get(
-                'https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info')
+        logging.info("Checking...")
+        get_info = self.session.get('https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info')
         last_info = get_info.json()
 
-        logging.info("上一次提交日期为: %s " % last_info["d"]["info"]["date"])
+        logging.info("Last data: %s " % last_info["d"]["info"]["date"])
 
         position = last_info["d"]["info"]['geo_api_info']
         position = json_loads(position)
 
-        logging.info("上一次提交地址为: %s" % position['formattedAddress'])
-        # logging.debug("上一次提交GPS为", position["position"])
+        logging.info("Last address: %s" % position['formattedAddress'])
 
         today = time.strftime("%Y%m%d", time.localtime())
-
+        
+        global SUCCESS
         if last_info["d"]["info"]["date"] == today:
-            logging.info("今日已提交")
-            self.close()
+            logging.info("Already Submitted.")
+            SUCCESS = True
         else:
-            logging.info("未提交")
+            logging.info("NOT yet submitted.")
+            SUCCESS = False
             self.last_info = last_info["d"]["info"]
 
-    def checkin(self):
+    def submit(self):
         """
         提交
         """
         headers = {
-            "Host"      : "zlapp.fudan.edu.cn",
-            "Referer"   : "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily?from=history",
-            "DNT"       : "1",
-            "TE"        : "Trailers",
+            "Origin"    : "https://zlapp.fudan.edu.cn",
+            "Referer"   : "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily",
             "User-Agent": self.UA
         }
 
-        logging.debug("提交中")
+        logging.info("Submitting...")
 
         geo_api_info = json_loads(self.last_info["geo_api_info"])
         province = geo_api_info["addressComponent"].get("province", "")
         city = geo_api_info["addressComponent"].get("city", "") or province
         district = geo_api_info["addressComponent"].get("district", "")
+        if city == province:
+            area = " ".join(set((province, district)))
+        else:
+            area = " ".join(set((province, city, district)))
         self.last_info.update(
                 {
                     "tw"      : "13",
                     "province": province,
                     "city"    : city,
-                    "area"    : " ".join(set((province, city, district))),
+                    "area"    : area,
                     "ismoved" : 0
                 }
         )
-        # logging.debug(self.last_info)
 
         save = self.session.post(
                 'https://zlapp.fudan.edu.cn/ncov/wap/fudan/save',
@@ -185,24 +183,38 @@ class Zlapp(Fudan):
         save_msg = json_loads(save.text)["m"]
         logging.info(save_msg)
 
-def get_account():
-    """
-    获取账号信息
-    """
-    uid, psw = sys_argv[1].strip().split(' ')
-    return uid, psw
+def send_mail():
+    logging.info('Sending email...')
+    mail_sender, mail_psw, mail_receiver = sys_argv[2].strip().split(' ')
+    mail_host = 'smtp.yeah.net'
+
+    message = MIMEText('打卡失败', 'plain', 'utf-8')
+    message['From'] = mail_sender.split('@')[0] + '<' + mail_sender + '>'
+    message['To'] = mail_receiver.split('@')[0] + '<' + mail_receiver + '>'
+    message['Subject'] = '平安复旦打卡失败通知'
+    
+    try:
+        smtpObj = smtplib.SMTP() 
+        smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
+        smtpObj.login(mail_sender, mail_psw)  
+        smtpObj.sendmail(mail_sender, mail_receiver, message.as_string())
+        logging.info('Email sent.')
+    except:
+        logging.error('Failed to send email.')
 
 if __name__ == '__main__':
-    uid, psw = get_account()
-    # logging.debug("ACCOUNT：" + uid + psw)
-    zlapp_login = 'https://uis.fudan.edu.cn/authserver/login?' \
-                  'service=https://zlapp.fudan.edu.cn/site/ncov/fudanDaily'
-    daily_fudan = Zlapp(uid, psw, url_login=zlapp_login)
-    daily_fudan.login()
-
-    daily_fudan.check()
-    daily_fudan.checkin()
-    # 再检查一遍
-    daily_fudan.check()
-
-    daily_fudan.close()
+    try:
+        uid, psw = sys_argv[1].strip().split(' ')
+        daily_fudan = Zlapp(uid, psw)
+        daily_fudan.login()
+        daily_fudan.check()
+        if not SUCCESS:
+            daily_fudan.submit()
+            daily_fudan.check()
+        daily_fudan.close()
+    except:
+        logging.exception('打卡失败！')
+        SUCCESS = False
+    
+    if not SUCCESS:
+        send_mail()
